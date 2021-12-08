f52feb7e8296139f4d0011ddb989def367a0f7da
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2021-12-08 17:52:37 -0500

    perf: :zap: stop brute forcing

--- src/factors.ts
+++ src/factors.ts
@@ -20,9 +20,57 @@ export function lcm(a: number, b: number): number {
     throw new Error("a and b must be positive");
   }
 
-  for (let i = 1; true; i++) {
-    if (i % a === 0 && i % b === 0) {
-      return i;
+  let gcf = 1;
+
+  // for every prime (and also some other numbers)
+  for (const n of primeNumbers()) {
+    /*
+      loop invariant: a and b are not both divisible by any value less than n
+        ∀x(x < n -> ¬(a divisible by x) ∨ ¬(b divisible by x))
+    */
+
+    if (n > a || n > b) {
+      /*
+        ∀x∀y(x > y -> ¬(y divisible by x))
+        ∴ a and b are coprime
+      */
+      break;
     }
+
+    while (a % n === 0 && b % n === 0) {
+      gcf *= n;
+      a /= n;
+      b /= n;
+    }
+  }
+  /*
+    gcf` = the greatest common factor of a and b
+    a` = `a / gcf`
+    b` = `b / gcf`
+  */
+
+  return gcf * a * b;
+  /*
+    return = a` * b` * gcf`
+      = `a * `b / gcf`
+      = the least common multiple of a and b
+  */
+}
+
+/**
+ * @yields a sequence of numbers that includes all prime numbers
+ *
+ * @note this function also yields numbers 9 and 15 (which are not prime)
+ *
+ * @example
+ * 2, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, ...
+ */
+function* primeNumbers(): IterableIterator<number> {
+  // 2 is the only even prime number
+  yield 2;
+
+  // all odd numbers starting at 3
+  for (let n = 3; true; n += 2) {
+    yield n;
   }
 }
