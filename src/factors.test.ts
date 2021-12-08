c0cf6f9ba079c5d4057dc2cbfe25444ae2075a16
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2021-12-08 00:46:55 -0500

    test: :test_tube: add tests and examples

--- src/factors.test.ts
+++ src/factors.test.ts
@@ -1,4 +1,72 @@
-import { assertEquals } from "https://deno.land/std@0.117.0/testing/asserts.ts";
+import { assertEquals, assertThrows } from "https://deno.land/std@0.117.0/testing/asserts.ts";
 
 import { lcm } from "./factors.ts";
 
+Deno.test("LCM of 1 and 1", () => {
+  assertEquals(lcm(1, 1), 1, "lcm(1, 1) should be 1");
+});
+Deno.test("LCM of 1 and 2", () => {
+  assertEquals(lcm(1, 2), 2, "lcm(1, 2) should be 2");
+});
+
+Deno.test("LCM of 2 and 3", () => {
+  assertEquals(lcm(2, 3), 6, "lcm(2, 3) should be 6");
+});
+
+Deno.test("LCM of 10 and 15", () => {
+  assertEquals(lcm(10, 15), 30, "lcm(10, 15) should be 30");
+});
+
+Deno.test("LCM of 100 and 600", () => {
+  assertEquals(lcm(100, 600), 600, "lcm(100, 600) should be 600");
+});
+
+Deno.test("LCM of 1 and 0", () => {
+  assertThrows(
+    () => {
+      lcm(1, 0);
+    },
+    Error,
+    "can not find the lcm of a non-positive number"
+  );
+});
+
+Deno.test("LCM of 0 and 1", () => {
+  assertThrows(
+    () => {
+      lcm(0, 1);
+    },
+    Error,
+    "can not find the lcm of a non-positive number"
+  );
+});
+
+Deno.test("LCM of 1 and -1", () => {
+  assertThrows(
+    () => {
+      lcm(1, -1);
+    },
+    Error,
+    "can not find the lcm of a non-positive number"
+  );
+});
+
+Deno.test("LCM of 1 and 1.5", () => {
+  assertThrows(
+    () => {
+      lcm(1, 1.5);
+    },
+    Error,
+    "can not find the lcm of a non-integer number"
+  );
+});
+
+Deno.test("LCM of 1.5 and 1", () => {
+  assertThrows(
+    () => {
+      lcm(1.5, 1);
+    },
+    Error,
+    "can not find the lcm of a non-integer number"
+  );
+});
--- src/factors.ts
+++ src/factors.ts
@@ -4,6 +4,12 @@
  * @param a the first positive integer
  * @param b the second positive integer
  * @returns the least common multiple of a and b
+ *
+ * @example
+ * lcm(2, 3) // returns 6
+ * lcm(10, 15) // returns 30
+ * lcm(1, -1) // throws an error
+ * lcm(1, 1.5) // throws an error
  */
 export function lcm(a: number, b: number): number {
   throw new Error("Method not implemented."); // TODO
