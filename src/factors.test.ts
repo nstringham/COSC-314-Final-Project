ba99204d3e5ab6f139dd5e9a7848fa2aec0938b0
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2021-12-08 00:29:01 -0500

    feat: :sparkles: add lcm function scafold

---
+++ src/factors.test.ts
@@ -0,0 +1,4 @@
+import { assertEquals } from "https://deno.land/std@0.117.0/testing/asserts.ts";
+
+import { lcm } from "./factors.ts";
+
---
+++ src/factors.ts
@@ -0,0 +1,10 @@
+/**
+ * Finds the least common multiple of two positive integers.
+ *
+ * @param a the first positive integer
+ * @param b the second positive integer
+ * @returns the least common multiple of a and b
+ */
+export function lcm(a: number, b: number): number {
+  throw new Error("Method not implemented."); // TODO
+}
