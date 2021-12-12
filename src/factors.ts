db720ebfe517f6a39907de7bf460309b4058d5da
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2021-12-12 02:54:13 -0500

    perf: :zap: use Stein's algorithm for gcf

--- src/factors.ts
+++ src/factors.ts
@@ -9,64 +9,80 @@
  * lcm(2, 3) // returns 6
  * lcm(10, 15) // returns 30
  * lcm(1, -1) // throws an error
- * lcm(1, 1.5) // throws an error
  */
 export function lcm(a: bigint, b: bigint): bigint {
   if (a <= 0 || b <= 0) {
     throw new Error("a and b must be positive");
   }
 
-  let gcf = 1n;
-
-  // for every prime (and also some other numbers)
-  for (const n of primeNumbers()) {
-    /*
-      loop invariant: a and b are not both divisible by any value less than n
-        ∀x(x < n -> ¬(a divisible by x) ∨ ¬(b divisible by x))
-    */
-
-    if (n > a || n > b) {
-      /*
-        ∀x∀y(x > y -> ¬(y divisible by x))
-        ∴ a and b are coprime
-      */
-      break;
-    }
-
-    while (a % n === 0n && b % n === 0n) {
-      gcf *= n;
-      a /= n;
-      b /= n;
-    }
-  }
-  /*
-    gcf` = the greatest common factor of a and b
-    a` = `a / gcf`
-    b` = `b / gcf`
-  */
-
-  return gcf * a * b;
-  /*
-    return = a` * b` * gcf`
-      = `a * `b / gcf`
-      = the least common multiple of a and b
-  */
+  return (a * b) / gcf(a, b);
 }
 
 /**
- * @yields a sequence of numbers that includes all prime numbers
+ * Finds the greatest common factor of two non-zero integers using Stein’s Algorithm.
  *
- * @note this function also yields numbers 9 and 15 (which are not prime)
+ * @param a the first integer
+ * @param b the second integer
+ * @returns the greatest common factor of a and b
  *
  * @example
- * 2, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, ...
+ * gcf(2, 3) // returns 1
+ * gcf(10, 15) // returns 5
+ * gcf(1, -1) // throws an error
+ *
+ * @see https://en.wikipedia.org/wiki/Binary_GCD_algorithm
+ *
+ * @credit adapted from https://www.geeksforgeeks.org/steins-algorithm-for-finding-gcd
  */
-function* primeNumbers(): IterableIterator<bigint> {
-  // 2 is the only even prime number
-  yield 2n;
-
-  // all odd numbers starting at 3
-  for (let n = 3n; true; n += 2n) {
-    yield n;
+export function gcf(a: bigint, b: bigint): bigint {
+  if (a < 0 || b < 0) {
+    throw new Error("a and b cannot be negative");
   }
+
+  // the gcf of 0 and any number is that number
+  if (a === 0n) {
+    return b;
+  } else if (b === 0n) {
+    return a;
+  }
+  // a > 0 ∧ b > 0
+
+  // apply the gcf(2a, 2b) = 2gcf(a, b) rule k times
+  let k = 0n;
+  while (((a | b) & 1n) === 0n) {
+    a >>= 1n;
+    b >>= 1n;
+    k++;
+  }
+  // a is odd ∨ b is odd
+
+  while ((a & 1n) === 0n) {
+    // a is even => b is odd
+
+    // gcf(2a, b) = gcf(a, b)
+    a >>= 1n;
+  }
+  // a is odd
+
+  do {
+    // gcf(a, 2b) = gcf(a, b)
+    while ((b & 1n) === 0n) {
+      b >>= 1n;
+    }
+
+    // gcf(a, b) = gcf(|a − b|, min(a, b))
+
+    // this sets a = min(a, b) and b = max(a, b)
+    if (a > b) {
+      const temp = a;
+      a = b;
+      b = temp;
+    }
+
+    // b = |a - b| = max(a, b) - min(a, b)
+    b = b - a;
+  } while (b !== 0n);
+
+  // multiply the 2^k back in from the gcf(2a, 2b) = 2gcf(a, b) rule
+  return a << k;
 }
